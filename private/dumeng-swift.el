c2cd42d6d64257be6bacb38037b26bce01f98533
dumeng <dumeng81@outlook.com>
2017-03-22 10:50:16 +0800

    add swift template mode

--- private/dumeng-swift.el
+++ private/dumeng-swift.el
@@ -4,9 +4,17 @@
 
 ;;; Code:
 
+(require-package 'swift-mode)
+(require 'swift-mode)
 
-(require 'swift)
+
+(require-package 'web-mode)
+(require 'web-mode)
+
+(add-hook 'web-mode-hook (lambda () (emmet-mode 1)))
+(add-to-list 'auto-mode-alist '("\\.stencil" . web-mode))
+
 
 
 (provide 'dumeng-swift)
-;;; dumeng-swift ends here
+;;; dumeng-swift.el ends here
