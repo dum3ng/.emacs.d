3470e0409c4abe22e7423e50df9834a7f3fd1da4
dumeng <dumeng81@outlook.com>
2017-03-23 11:58:08 +0800

    use web mode to edit .scss file

--- private/dumeng-css.el
+++ private/dumeng-css.el
@@ -7,5 +7,10 @@
 (after-load 'scss
   (setq css-indent-offset 2))
 
+(require-package 'web-mode)
+(add-auto-mode 'web-mode "\\.scss")
+(add-hook 'web-mode-hook (lambda () (progn (rainbow-delimiters-mode-enable)
+                                      (aggressive-indent-mode 1))))
+
 (provide 'dumeng-css)
 ;;; dumeng-css ends here
